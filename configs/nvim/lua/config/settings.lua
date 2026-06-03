74947f3f286ecf0c5b94008a208b6daad4d4f5d4
shao <shao@users.noreply.github.com>
2026-06-03 20:33:04 +0800

    feat: tmux 快捷应用 + nvim 编码/鼠标/光标位置配置

--- configs/nvim/lua/config/settings.lua
+++ configs/nvim/lua/config/settings.lua
@@ -10,6 +10,12 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = true
+vim.opt.autoindent = true
+
+-- encoding
+vim.opt.fileencodings = "utf-8"
+vim.opt.termencoding = "utf-8"
+vim.opt.encoding = "utf-8"
 
 vim.opt.wrap = false
 vim.opt.ignorecase = true
@@ -21,7 +27,7 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 
 vim.opt.termguicolors = true
-vim.opt.mouse = "a"
+vim.opt.mouse = ""
 vim.opt.clipboard = "unnamedplus"
 vim.opt.updatetime = 300
 vim.opt.timeoutlen = 300
@@ -33,3 +39,14 @@ vim.opt.writebackup = false
 
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
+
+-- 记住上次光标位置
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = vim.api.nvim_create_augroup("RestoreCursor", { clear = true }),
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    if mark[1] > 0 and mark[1] <= vim.api.nvim_buf_line_count(0) then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
